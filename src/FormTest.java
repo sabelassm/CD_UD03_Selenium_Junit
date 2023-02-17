4864c9c7dcc5e3b6061efe7570bab07b1a945646
Sabela <sabela@iessanclemente.net>
2023-02-17 14:10:26 +0100

    elementos form

--- src/FormTest.java
+++ src/FormTest.java
@@ -1,22 +1,57 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
+
+//https://codepaster.net/app.html#/student/alani-71231
 public class FormTest {
 
     WebDriver driver;
     
     @Before
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");  
+        System.setProperty("webdriver.chrome.driver", "/home/sanclemente.local/sabela/Escritorio/CD_UD03_Selenium/driver/chromedriver");  
         driver = new ChromeDriver(); 
-        driver.get("src/index.html");
     }
 
     @Test
     public void testCombo(){
-        driver.findElement(By.tagName("input")).getAttribute("type");
+
+        driver.get("file:///home/sanclemente.local/sabela/Escritorio/CD_UD03_Selenium/src/index.html");
+       
+        WebElement checkboxPinha = driver.findElement(By.name("pinha"));
+        WebElement checkboxSinPinha = driver.findElement(By.name("pinha"));
+
+        checkboxPinha.click();
+        checkboxSinPinha.click();
+        assertEquals(true, checkboxPinha.isSelected());
+        assertEquals(false, checkboxSinPinha.isSelected());
+    }
+
+    @Test
+    public void testRadio(){
+        driver.get("file:///home/sanclemente.local/sabela/Escritorio/CD_UD03_Selenium/src/index.html");
+
+        WebElement masaFina = driver.findElement(By.cssSelector("[name='boton' value='1']"));
+        masaFina.click(); 
+        WebElement masaGruesa = driver.findElement(By.xpath("//input[@name='boton' and @value='2']"));
+        masaGruesa.click();
+    }
+
+    @Test
+    public void testComo(){
+        driver.get("file:///home/sanclemente.local/sabela/Escritorio/CD_UD03_Selenium/src/index.html");
+
+        Select menu = new Select(driver.findElement(By.name("menu")));
+        menu.selectByIndex(0);
+        menu.selectByValue("2");
+        menu.selectByVisibleText("Tres");
+        
     }
 }
